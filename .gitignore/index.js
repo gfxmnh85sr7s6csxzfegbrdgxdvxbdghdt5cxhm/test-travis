9b3feff1f4fdc280a537ca87ae74955cab779679
rtste45trtd545 <16viko+github@gmail.com>
2018-06-11 15:52:14 +0800

    Delete index.js

--- .gitignore/index.js
+++
@@ -1,6 +0,0 @@
-const { execSync } = require('child_process')
-const shellOpts = {
-  stdio: 'inherit',
-  shell: true
-}
-execSync('ghq', [ 'get', '-p', 'https://github.com/npm/node-which' ], shellOpts)
